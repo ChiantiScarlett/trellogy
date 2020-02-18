e055d5edd02e178d6b7b723eb8f4e938453593ec
Chianti Scarlett <chianti.scarlett@gmail.com>
2020-02-18 19:17:03 +0900

    typo fix

--- trellogy/error.py
+++ trellogy/error.py
@@ -6,7 +6,7 @@ class InvalidAPIError(Exception):
         return "Invalid API KEY or API TOKEN."
 
 
-class NoCardFoundError(ExcepTion):
+class NoCardFoundError(Exception):
     def __init__(self, board_id):
         self.board_id = board_id
 
