e958f16518a73e11a58086a02ada8f09090f22b3
Chianti Scarlett <chianti.scarlett@gmail.com>
2020-02-20 00:33:11 +0900

    add get_lists method

--- trellogy/trellogy.py
+++ trellogy/trellogy.py
@@ -1,19 +1,36 @@
 from .components import List, Card
-from .error import InvalidAPIError, NoListFoundError
+from .error import InvalidAPIError, InvalidListIDError
+import requests
 
 
 class Trellogy:
-    def __init__(self, API_KEY, API_TOKEN, board_id, trash_id=None):
-        self.board_id = board_id
-        self.trash_id = trash_id
+    def __init__(self, key, token, board_id, trash_id=None):
+        self._key = key
+        self._token = token
+        self._board_id = board_id
+        self._trash_id = trash_id
 
     def create_list(self, title) -> Card:
         pass
 
-    def read_list(self, list_id: str) -> List:
-        pass
+    def get_lists(self) -> list:
+        response = requests.get(
+            'https://api.trello.com/1/boards/' +
+            '{BOARD_ID}/lists?key={KEY}&token={TOKEN}'.format(
+                BOARD_ID=self._board_id, KEY=self._key, TOKEN=self._token
+            ))
+        if response.status_code != 200:
+            raise InvalidListIDError
 
-    def read_lists(self) -> list:
+        lists = []
+        for list_item in response.json():
+            lists.append(
+                List(key=self._key, token=self._token, board_id=self._board_id,
+                     trash_id=self._trash_id, list_id=list_item['id'],
+                     name=list_item['name']))
+        return lists
+
+    def get_list(self, list_id: str) -> List:
         pass
 
     def create_card(self) -> Card:
