3745d5e0ea3df877181401d8360aae1fc04462b7
Chianti Scarlett <chianti.scarlett@gmail.com>
2020-02-21 18:16:04 +0900

    remove unnecessary errors

--- trellogy/error.py
+++ trellogy/error.py
@@ -1,3 +1,11 @@
+class InvalidParamError(Exception):
+    def __init__(self, param):
+        self._param = param
+
+    def __str__(self):
+        return "Invalid parameter `{}`".format(self._param)
+
+
 class TrellogyError(Exception):
     def __init__(self, message):
         self._message = message
@@ -12,35 +20,3 @@ class NotEnoughParamsError(Exception):
 
     def __str__(self):
         return "Value for the key `{}` was not given.".format(self._key)
-
-
-class InvalidKeyTokenError(Exception):
-    def __init__(self):
-        pass
-
-    def __str__(self):
-        return "Invalid API KEY or API TOKEN."
-
-
-class InvalidBoardIDError(Exception):
-    def __init__(self, board_id):
-        self.board_id = board_id
-
-    def __str__(self):
-        return "No board was found by the id `{}`.".format(self.board_id)
-
-
-class InvalidListIDError(Exception):
-    def __init__(self, list_id):
-        self.list_id = list_id
-
-    def __str__(self):
-        return "No list was found by the id `{}`.".format(self.list_id)
-
-
-class InvalidCardIDError(Exception):
-    def __init__(self, card_id):
-        self.card_id = card_id
-
-    def __str__(self):
-        return "No card was found by the id `{}`.".format(self.card_id)
