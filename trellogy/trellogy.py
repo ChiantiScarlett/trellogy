c0eed00b5a45008c90f4524531886fd3270a8a7d
Chianti Scarlett <chianti.scarlett@gmail.com>
2020-02-21 18:15:43 +0900

    prettify codes

--- trellogy/trellogy.py
+++ trellogy/trellogy.py
@@ -1,5 +1,5 @@
 from .components import List, Card
-from .error import TrellogyError, InvalidListIDError
+from .error import TrellogyError
 import requests
 
 
@@ -8,21 +8,25 @@ class Trellogy:
         self._key = key
         self._token = token
         self._board_id = board_id
-        self._trash_id = trash_id
+        self._idTrash = trash_id
 
-    def create_list(self, title: str, position='bottom') -> List:
+    def create_list(self, title: str, pos='bottom') -> List:
         response = requests.post(
             'https://api.trello.com/1/boards/' +
             '{BOARD_ID}/lists?name={TITLE}&pos={POSITION}&key={KEY}&token={TOKEN}'.format(
                 BOARD_ID=self._board_id,
-                TITLE=title, POSITION=position,
+                TITLE=title, POSITION=pos,
                 KEY=self._key, TOKEN=self._token,
             )
         )
         if response.status_code != 200:
             raise TrellogyError(response.json()['message'])
 
-        return response.json()
+        new_list = response.json()
+        return List(key=self._key,
+                    token=self._token,
+                    idTrash=self._idTrash,
+                    **new_list)
 
     def get_lists(self) -> list:
         response = requests.get(
@@ -31,21 +35,29 @@ class Trellogy:
                 BOARD_ID=self._board_id, KEY=self._key, TOKEN=self._token
             ))
         if response.status_code != 200:
-            raise InvalidListIDError
+            raise TrellogyError(response.text)
 
         lists = []
         for list_item in response.json():
             lists.append(
-                List(key=self._key, token=self._token, board_id=self._board_id,
-                     trash_id=self._trash_id, list_id=list_item['id'],
-                     name=list_item['name']))
+                List(key=self._key,
+                     token=self._token,
+                     idTrash=self._idTrash,
+                     **list_item))
         return lists
 
     def get_list(self, list_id: str) -> List:
-        pass
+        url = 'https://api.trello.com/1/lists' + \
+            '/{LIST_ID}?key={KEY}&token={TOKEN}'.format(
+                BOARD_ID=self._board_id, KEY=self._key, TOKEN=self._token,
+                LIST_ID=list_id
+            )
+        response = requests.get(url)
 
-    def create_card(self) -> Card:
-        pass
+        if response.status_code != 200:
+            raise TrellogyError(response.text)
 
-    def read_card(self, card_id: str) -> Card:
-        pass
+        return List(key=self._key,
+                    token=self._token,
+                    idTrash=self._idTrash,
+                    **response.json())
