aaf19b8c180796add8b3d306c9763f14744d94e9
Chianti Scarlett <chianti.scarlett@gmail.com>
2020-02-21 09:26:16 +0900

    add create_list method

--- trellogy/trellogy.py
+++ trellogy/trellogy.py
@@ -1,5 +1,5 @@
 from .components import List, Card
-from .error import InvalidAPIError, InvalidListIDError
+from .error import TrellogyError, InvalidListIDError
 import requests
 
 
@@ -10,8 +10,19 @@ class Trellogy:
         self._board_id = board_id
         self._trash_id = trash_id
 
-    def create_list(self, title) -> Card:
-        pass
+    def create_list(self, title: str, position='bottom') -> List:
+        response = requests.post(
+            'https://api.trello.com/1/boards/' +
+            '{BOARD_ID}/lists?name={TITLE}&pos={POSITION}&key={KEY}&token={TOKEN}'.format(
+                BOARD_ID=self._board_id,
+                TITLE=title, POSITION=position,
+                KEY=self._key, TOKEN=self._token,
+            )
+        )
+        if response.status_code != 200:
+            raise TrellogyError(response.json()['message'])
+
+        return response.json()
 
     def get_lists(self) -> list:
         response = requests.get(
