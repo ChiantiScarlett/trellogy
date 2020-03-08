f24a8a8ea4fabbb48dc27a06a45a871b0a926b97
Chianti Scarlett <chianti.scarlett@gmail.com>
2020-03-09 01:05:06 +0900

    add labels property

--- trellogy/board.py
+++ trellogy/board.py
@@ -1,37 +1,23 @@
 from .error import TrellogyError
 from .component import Component
-from .list import List
-import requests
+from .label import Label
 
 
 class Board(Component):
     def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-        self._name = None
-
-    def create_list(self):
-        pass
+        _attributes = []
+        super().__init__(**kwargs, _attributes=_attributes)
 
     @property
-    def lists(self):
-        response = requests.get(
-            'https://api.trello.com/1/boards/' +
-            '{BOARD_ID}/lists?key={KEY}&token={TOKEN}'.format(
-                BOARD_ID=self._id, KEY=self._key, TOKEN=self._token
-            ))
-        if response.status_code != 200:
-            raise TrellogyError(response.text)
-
-        lists = []
-        for list_item in response.json():
-            lists.append(
-                List(key=self._key,
-                     token=self._token,
-                     id=list_item['id'],
-                     name=list_item['name'],
-                     closed=list_item['closed'],
-                     board_id=list_item['idBoard'],
-                     position=list_item['pos']
-                     ))
-        return lists
+    def labels(self):
+        path = '/boards/{BOARD_ID}/labels'.format(BOARD_ID=self._id)
+        response = self.req(path)
+        labels = []
+        for label in response:
+            labels.append(Label(key=self._key,
+                                token=self._token,
+                                id=label['id'],
+                                board_id=label['idBoard'],
+                                name=label['name'],
+                                color=label['color']))
+        return labels
