0ef8e2a2284c138c3dffc8e6f13eb16e2c607c37
Chianti Scarlett <chianti.scarlett@gmail.com>
2020-03-10 01:46:57 +0900

    fix update method

--- trellogy/card.py
+++ trellogy/card.py
@@ -8,32 +8,19 @@ class Card(Component):
         _attributes = ['id', 'closed', 'name', 'desc', 'labels']
         super().__init__(**kwargs, _attributes=_attributes)
 
-    def update(self, name=None, closed=None, desc=None, labels=None):
+    def update(self, name=None, closed=None, desc=None, labels=None, board_id=None):
 
         name = self._name if name is None else name
         closed = self._closed if closed is None else closed
         desc = self._desc if desc is None else desc
         labels = self._labels if labels is None else labels
+        board_id = self._board_id if board_id is None else board_id
 
         # Update labels
         labels_id = ",".join([label.id for label in labels])
         self.req('PUT', '/cards/{}'.format(self._id),
                  name=name, closed=self.bool_to_str[closed],
-                 desc=quote(desc), idLabels=labels_id)
-
-        # name = self._name if not name else name
-        # closed = {True: 'true', False: 'false'}[
-        #     self._closed] if not closed else closed
-        # board_id = self._board_id if not board_id else board_id
-        # position = self._position if not position else position
-
-        # self.req('PUT', '/lists/{}'.format(self._id),
-        #          name=name, closed=closed,
-        #          board_id=board_id, position=position)
-
-    def delete(self):
-        pass
-        # self.req('DELETE', '/labels/{}'.format(self._id))
+                 desc=quote(desc), idLabels=labels_id, idBoard=board_id)
 
     @property
     def cards(self):
